fad987504570b649a62e9a1c887381878e39b5bc
Khanittha-git <tarn.khanit@odds.team>
2020-09-13 15:00:03 +0700

    [ADD] chapter1 Hello

---
+++ src/golang-book/chapter2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, my name is Tarn")
+}
+
+//this is a comment
+//read up to down
+/* 	1.package declaration.
+   	2.import code from another package to my program and fmt is format.
+	3.in double quotes use for know in " "is a expression.
+	4.Println is print one by one line //read more : command > go doc fmt.Println
+*/
